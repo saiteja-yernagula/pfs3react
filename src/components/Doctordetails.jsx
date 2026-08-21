4452ae7ac9a92e48c5f3815763ffcfa2b33ea718
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-08-21 10:03:37 +0530

    sdfdf

--- src/components/Doctordetails.jsx
+++ src/components/Doctordetails.jsx
@@ -1,40 +1,13 @@
 import { useParams } from 'react-router-dom'
 import { useState,useEffect } from 'react'
+import axios from 'axios';
 function Doctordetails() {
   let [doctor,setDoctor]=useState({})
   let {id}=useParams()
 
-  function getapidata(){
-    let data = [
-      {
-        id: 1,
-        name: "Teja",
-        age: 26,
-        gender: "Male",
-        specialization: "Muscles",
-        salary: 7000000,
-      },
-
-      {
-        id: 2,
-        name: "Sam",
-        age: 26,
-        gender: "Male",
-        specialization: "Bones",
-        salary: 4000000,
-      },
-
-      {
-        id: 3,
-        name: "Anu",
-        age: 25,
-        gender: "Female",
-        specialization: "Heart",
-        salary: 5000000,
-      }
-    ];
-
-    let finaldata=data.find((val)=>val.id==id)
+  async function getapidata(){
+    let response=await axios.get('https://doctorapibackend.onrender.com/doctors')//{}
+    let finaldata=response.data.find((val)=>val.id==id)
     setDoctor(finaldata)
     console.log(finaldata)
   }
